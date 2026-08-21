36e828bed87678983b9c7c59f7683816e1ec7210
Wei Chi Chen (陳瑋棋) <eddie067@hotmail.com>
2026-08-22 01:04:39 +0800

    research: export M4 universe primitives

--- server/poc_absorption/__init__.py
+++ server/poc_absorption/__init__.py
@@ -9,6 +9,15 @@ from .bars import (
     build_developing_poc,
     volume_profile_levels,
 )
+from .universe import (
+    EVENT_SCHEMA_VERSION,
+    UNIVERSE_VERSION,
+    UNIVERSE_SCHEMA_VERSION,
+    HighPriceProbeConfig,
+    UniverseResult,
+    build_high_price_probe_universe,
+    first_trigger_per_episode,
+)
 
 __all__ = [
     "BAR_RESOLUTIONS",
@@ -18,4 +27,11 @@ __all__ = [
     "build_bars",
     "build_developing_poc",
     "volume_profile_levels",
+    "EVENT_SCHEMA_VERSION",
+    "UNIVERSE_VERSION",
+    "UNIVERSE_SCHEMA_VERSION",
+    "HighPriceProbeConfig",
+    "UniverseResult",
+    "build_high_price_probe_universe",
+    "first_trigger_per_episode",
 ]
